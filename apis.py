7d3a6cc190c454079c9c98532dab934f9d2fc8f8
Macr0phag3 <997334636@qq.com>
2019-04-01 16:29:42 +0800

    upload latest code

--- apis.py
+++ apis.py
@@ -9,7 +9,7 @@ import toolbox
 - api 函数命名：api_xxxx
 
 - api 函数必须返回 json，示例:
-dicts = {
+results = {
     "code": 0,
     "msg": "",
     "result": "" # 与 slave 返回的值一致
@@ -43,6 +43,36 @@ def api_getContainerList():
             }
     }
 
-    return toolbox.send_mission('192.168.12.1', mission)
+    results = {
+        "code": 1,
+        "msg": "",
+        "result": {
+            i: {
+                'code': 1,
+                'msg': '',
+                'result': []
+            } for i in setting["slave_ip"]
+        }
+    }
+
+    for ip in setting["slave_ip"]:
+        result = json.loads(toolbox.send_mission(ip, mission))
+        if result["code"]:
+            results['result']['ip']['msg'] = result['msg']
+        else:
+            results['result']['ip']['code'] = 0
+            results['result']['ip']['result'] = result['result']
+
+    return results
 
 
+# ------------- 载入配置 -------------
+try:
+    with open(".setting", "r") as fp:
+        setting = json.load(fp)
+except Exception, e:
+    print pt.put_color(u"载入配置出错", "red")
+    print str(e)
+    pt.log(traceback.format_exc(), level="error",
+           description="load setting failed!", path=".slave_log")
+    raise
