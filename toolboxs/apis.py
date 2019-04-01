94e3f033e59cc91166f6dbc5e122592b62579cab
Macr0phag3 <997334636@qq.com>
2019-04-01 15:43:13 +0800

    upload latest code

--- toolboxs/apis.py
+++ toolboxs/apis.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 
 
-def _getImageList(mission, arg1, arg2):
-    return '{}'
+class API:
+    # 新增的 api 放在这
 
+    # !!! api 约定 !!!
+    '''
+    - api 命名：api_xxxx
 
-# 新增的 api 放在这
-# api 约定：
-'''
-- mission 格式为：
-{
-    "mission": {
-        "getImageList": ["test"]
+    - mission 格式为：
+    {
+        "mission": {
+            "getImageList": ["test"]
+        }
     }
-}
 
-- 必须返回 json
+    - api 必须返回 json
 
-'''
-apis = {
-    'getImageList': _getImageList,
-}
+    '''
+
+    def api_getImageList(mission):
+        return '{}'
+
+    def api_pullImage():
+        return '{}'
+
+
+apis = {i[4:]: getattr(API, i) for i in dir(API) if i[:4] == 'api_'}
