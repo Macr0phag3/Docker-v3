ee3fb32e759eac0d1ae040b86c8184b9e0b9e9ff
Macr0phag3 <997334636@qq.com>
2019-04-02 19:40:00 +0800

    upload latest code

--- apis.py
+++ apis.py
@@ -23,31 +23,92 @@ results = {
 # ------------- 镜像相关 -------------
 
 
-def api_getImageList():
-    return '{}'
+def api_getImagesList():
+    """
+    所有镜像的列表
+
+    返回值示例
+    dicts = {
+        "code": 1,
+        "msg": "",
+        "result": [
+            "image_name_1",
+            "image_name_2"
+        ]
+    }
+    """
+    results = {
+        "code": 0,
+        "msg": "",
+        "result": []
+    }
+
+    try:
+        results["result"] = json.loads(commands.getoutput(
+            'curl -s 127.0.0.1:5000/v2/_catalog'))['repositories']
+    except Exception, e:
+        toolbox.log(traceback.format_exc(), level="error",
+                    description="get all images failed", path=".slave_log")
+
+        results["code"] = 1
+        results["msg"] = "master(%s) report a error: %s" % (
+            setting["bridge"]["self_ip"], str(e))
+
+    return results
 
 
-def api_pullImage():
-    return '{}'
+def api_pullImages(image_names):
+    '''
+    拉取镜像
+    1. image_names: 镜像名，列表
+    '''
+    mission = {
+        "mission": "cmd2slave",  # 具体的任务
+        "commands": {
+            "command": "pull_images",  # 具体的命令
+            "arg": image_names,  # 参数列表
+        }
+    }
+
+    results = {
+        "code": 0,
+        "msg": "",
+        "result": {
+            i: {
+                'code': 0,
+                'msg': '',
+                'result': ''
+            } for i in setting["slave_ip"]
+        }
+    }
+
+    for ip in ['192.168.12.1']:  # setting["slave_ip"]:
+        result = json.loads(toolbox.send_mission(ip, mission))
+        if result["code"]:
+            results['result'][ip]['code'] = 1
+            results['result'][ip]['msg'] = result['msg']
+        else:
+            results['result'][ip]['result'] = result['result']
+
+    return results
 
 
-def api_deleteImage():
+def api_deleteImages():
     return '{}'
 
 # ------------- 容器相关 -------------
 
 
-def api_getContainerList():
+def api_getContainersList():
     '''
     所有容器的列表
     '''
     mission = {
         "mission": "cmd2docker",  # 具体的任务
-        "commands":
-            {
-                "command": "containers_ls",  # 具体的命令
-                "arg": [],  # 参数列表
-            }
+        "commands": {
+            "command": "containers_ls",  # 具体的命令
+            "arg": [],  # 参数列表
+        }
     }
 
     results = {
@@ -72,71 +133,6 @@ def api_getContainerList():
 
     return results
 
-
-def api_getImageList():
-    """
-    所有镜像的列表
-
-    返回值示例
-    dicts = {
-        "code": 1,
-        "msg": "",
-        "result": [
-            "image_name_1",
-            "image_name_2"
-        ]
-    }
-    """
-    results = {
-        "code": 0,
-        "msg": "",
-        "result": []
-    }
-
-    try:
-        results["result"] = json.loads(commands.getoutput('curl -s 127.0.0.1:5000/v2/_catalog'))['repositories']
-    except Exception, e:
-        toolbox.log(traceback.format_exc(), level="error",
-                    description="get all images failed", path=".slave_log")
-
-        results["code"] = 1
-        results["msg"] = "master(%s) report a error: %s" % (
-            setting["bridge"]["self_ip"], str(e))
-
-    return results
-
-def api_pullImageList():
-    """
-    所有镜像的列表
-
-    返回值示例
-    dicts = {
-        "code": 1,
-        "msg": "",
-        "result": [
-            "image_name_1",
-            "image_name_2"
-        ]
-    }
-    """
-    dicts = {
-        "code": 0,
-        "msg": "",
-        "result": []
-    }
-
-    try:
-        dicts["result"] = [i.tags[0] for i in docker.from_env().images.list()]
-
-    except Exception, e:
-        toolbox.log(traceback.format_exc(), level="error",
-                    description="get all images failed", path=".slave_log")
-
-        dicts["code"] = 1
-        dicts["msg"] = "master(%s) report a error: %s" % (
-            setting["bridge"]["self_ip"], str(e))
-
-    return json.dumps(dicts)
 
 
 # ------------- 载入配置 -------------
